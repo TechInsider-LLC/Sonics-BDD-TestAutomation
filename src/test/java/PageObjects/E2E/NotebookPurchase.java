a55d7fbf0538788a82cb3025c701a10ae6a483d9
Munavvar <munavvar.balata@gmail.com>
2022-12-15 23:02:49 -0500

    NotebbokPurchase

---
+++ src/test/java/PageObjects/E2E/NotebookPurchase.java
@@ -0,0 +1,40 @@
+package PageObjects.E2E;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+import static utility.Hooks.driver;
+
+public class NotebookPurchase {
+    Actions action = new Actions(driver);
+    @FindBy(css = "[href=\"/computers\"]")
+    @CacheLookup
+    WebElement computers;
+
+
+    @FindBy(css = "[href=\"/notebooks\"]")
+    @CacheLookup
+    WebElement notebooks;
+
+    @FindBy(css = ".product-box-add-to-cart-button")
+    @CacheLookup
+    WebElement addtocart;
+
+    public static String url = "https://demowebshop.tricentis.com/";
+
+    public void home(){
+
+        driver.get(url);
+    }
+
+
+    public void cart(){
+
+        action.moveToElement(computers).perform();
+        notebooks.click();
+        addtocart.click();
+    }
+
+}
