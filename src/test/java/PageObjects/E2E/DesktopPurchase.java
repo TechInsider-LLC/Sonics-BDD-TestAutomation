45b5cc9a696f0ae2ab5fcfa0a2dd1a73b4acb028
Mukaddas23 <olimovna.2015@gmail.com>
2022-12-16 16:59:02 -0600

    DesktopPurchase

---
+++ src/test/java/PageObjects/E2E/DesktopPurchase.java
@@ -0,0 +1,57 @@
+package PageObjects.E2E;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+import static utility.Hooks.driver;
+
+public class DesktopPurchase {
+    Actions action = new Actions(driver);
+    @FindBy(css = "[href=\"/computers\"]")
+    @CacheLookup
+    WebElement computers;
+
+    @FindBy(css = "[href=\"/desktops\"]")
+    @CacheLookup
+    WebElement desktops;
+
+    @FindBy(xpath = "//*[@class=\"details\"]//*[text()=\"Build your own expensive computer\"]")
+    @CacheLookup
+    WebElement desktopImage;
+
+
+    @FindBy(css = "#product_attribute_74_5_26_82")
+    @CacheLookup
+    WebElement processor;
+
+    @FindBy(css = "#product_attribute_74_6_27_85")
+    @CacheLookup
+    WebElement ram;
+
+    @FindBy(css = "#product_attribute_74_3_28_87")
+    @CacheLookup
+    WebElement hdd;
+
+    @FindBy(css = "#add-to-cart-button-74")
+    @CacheLookup
+    WebElement addToCartButton;
+
+    //public String url= "https://demowebshop.tricentis.com/";
+
+
+
+    public void addToCartProcess(){
+        action.moveToElement(computers).perform();
+        desktops.click();
+        desktopImage.click();
+        processor.click();
+        ram.click();
+        hdd.click();
+        addToCartButton.click();
+
+
+
+    }
+}
---
+++ src/test/java/StepDefinitions/E2E/DesktopPurchase.java
@@ -0,0 +1,40 @@
+package StepDefinitions.E2E;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.PageFactory;
+import static utility.Hooks.driver;
+
+
+public class DesktopPurchase {
+    PageObjects.E2E.DesktopPurchase desktopPurchase = PageFactory.initElements(driver,PageObjects.E2E.DesktopPurchase.class);
+    PageObjects.ShoppingCart shoppingCart = PageFactory.initElements(driver,PageObjects.ShoppingCart.class);
+    PageObjects.Checkout checkout = PageFactory.initElements(driver,PageObjects.Checkout.class);
+    PageObjects.CheckOutAsGuest checkOutAsGuest= PageFactory.initElements(driver, PageObjects.CheckOutAsGuest.class);
+
+
+
+    @When("Guest User tries to purchase a Desktop")
+    public void guest_user_tries_to_purchase_a_desktop()  {
+
+        desktopPurchase.addToCartProcess();
+        shoppingCart.checkout();
+        checkOutAsGuest.checkOut();
+        checkout.checkoutAsGuest("Diana","Winter","asd@gmail.com","United States","Absdf","qwerty 22","123456","1234567890");
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
