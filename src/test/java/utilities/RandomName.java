5273e9395c7f55659657066b7607251e8637e027
MadinaMova <inogomovadina@gmail.com>
2022-12-21 17:47:45 -0500

    Revert "Revert "Merge pull request #37 from TechInsider-LLC/Update""
    This reverts commit ffe100cac300ffb7ffbda80af0abeb61e5acdd21.

--- src/test/java/utilities/RandomName.java
+++ src/test/java/utilities/RandomName.java
@@ -6,11 +6,11 @@ public class RandomName {
     public static String getRandomName(int nameCount) {
         String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         Random random = new Random();
-        StringBuilder name = new StringBuilder();
+        StringBuilder firstName = new StringBuilder();
         for (int i = 0; i < nameCount; i++) {
             char letter = abc.charAt(random.nextInt(26));
-            name.append(letter);
+            firstName.append(letter);
         }
-          return name.append("a").toString();
+          return firstName.append(firstName).toString();
     }
 }
