39fbff773224d5aa224dd18b10ed2921a68c929e
MadinaMova <inogomovadina@gmail.com>
2022-12-20 23:07:23 -0500

    update

--- src/test/java/PageObjects/UpdateCustomerInfo.java
+++ src/test/java/PageObjects/UpdateCustomerInfo.java
@@ -6,53 +6,53 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import utilities.RandomName;
 
 import static utilities.Hooks.driver;
 import static utilities.Hooks.wait;
 
 public class UpdateCustomerInfo {
     public String url = "https://demowebshop.tricentis.com/";
-    @FindBy(css=".header-links [href=\"/customer/info\"]")
+    @FindBy(css = ".header-links [href=\"/customer/info\"]")
     @CacheLookup
     WebElement myAccount;
 
-    @FindBy(css="#FirstName")
+    @FindBy(css = "#FirstName")
     @CacheLookup
     WebElement firstName;
 
-    @FindBy(css="#LastName")
+    @FindBy(css = "#LastName")
     @CacheLookup
     WebElement lastName;
 
-    @FindBy(css="#Email")
-    @CacheLookup
-    WebElement email;
-
-    @FindBy(css=".buttons ")
+    @FindBy(css = ".buttons")
     @CacheLookup
     WebElement saveButton;
-    @FindBy(css=".header-links [href=\"/customer/info\"]")
+
+    @FindBy(css = ".header-links [href=\"/customer/info\"]")
     @CacheLookup
     WebElement isUpdateAccountDisplayed;
-    public void homepage(){
+
+    public void homepage() {
+
         driver.get(url);
     }
-    public void with(String FirstName, String LastName){
 
+    public void with(String FirstName, String LastName) {
         myAccount.click();
-         firstName.clear();
-         firstName.sendKeys(FirstName);
-         lastName.clear();
-    lastName.sendKeys(LastName);
-   // email.clear();
-    //email.sendKeys(Email);
-   saveButton.click();
+        firstName.clear();
+        firstName.sendKeys(FirstName);
+        lastName.clear();
+        lastName.sendKeys(LastName);
+        saveButton.click();
     }
-   /* public static boolean isUpdateAccountDisplayed(){
 
+    /*  public static boolean isUpdateAccountDisplayed() {
+     *//* String expected = RandomName;
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".header-links [href=\"/customer/info\"]"))).getText();
-        String expected = "Alisa";
         String actual = UpdateCustomerInfo.getText();
         return expected.equals(actual);*/
-    }
+}
+
+
 
---
+++ src/test/java/features/UpdateCustomerInfo.feature
@@ -0,0 +1,6 @@
+Feature: Update Customer Info
+  Scenario: Registered User should be able to update Customer Info
+    Given Registered User is on the Homepage
+    When Registered User changes the Customer Info
+    Then Registered User gets Customer Info Changed
+
