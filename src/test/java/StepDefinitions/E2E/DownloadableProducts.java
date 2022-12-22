135141f9011b1c0718a10e4923b3c137f794684c
MadinaMova <inogomovadina@gmail.com>
2022-12-21 20:57:07 -0500

    Products

---
+++ src/test/java/StepDefinitions/E2E/DownloadableProducts.java
@@ -0,0 +1,16 @@
+package StepDefinitions.E2E;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DownloadableProducts {
+
+    @When("User purchases downloadable product")
+    public void user_purchases_downloadable_product() {
+
+    }
+    @Then("Verify the order number")
+    public void verify_the_order_number() {
+       
+    }
+}
